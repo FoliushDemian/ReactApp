fd5b8015dbb4ca6bac3c93e2a94c7d6bc6e37b71
Demian Foliush <Demian.Foliush.IR.2021@lpnu.ua>
2022-10-03 15:29:52 +0300

    initial commit

---
+++ src/layouts/header/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import logotype from "./../../assets/icons/logo.svg"
+import cart from "./../../assets/icons/cart.svg"
+import user from "./../../assets/icons/user.svg"
+import "./header.css"
+
+function Header() {
+    return (
+        <header>
+            <img src={logotype} className="logotype" />
+            <h1>Tabletki UA</h1>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">Catalog</a>
+                <a href="#">Cart</a>
+            </nav>
+            <img src={cart} className="cart"/>
+            <img src={user} className="user"/>
+        </header>
+    )
+}
+
+export default Header;
